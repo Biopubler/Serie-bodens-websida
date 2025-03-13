f1a708565c1ddb2f7030c78a6f17ecc641ed3dc6
Tor Karlsson <tor.karlsson@skola.taby.se>
2025-03-13 11:45:15 +0100

    Försökt fixa inlogg

--- Serieboden.js
+++ Serieboden.js
@@ -1,1 +1,9 @@
-// skriver en kommentar
+const Animationcss = document.getElementsByClassName("Animationcss");
+const formus = document.getElementsByClassName("formus");
+const Inloggning = document.getElementById("Inloggning");
+
+Animationcss.addEventListener("click", event =>{
+    formus.style.display = "none";
+    Inloggning.style.display = "none";
+    myButton.textContent = "Show";
+});
