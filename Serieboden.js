95372c2fbd129ec3d8f8a71fe58104acd9724a9a
Tor Karlsson <tor.karlsson@skola.taby.se>
2025-04-07 09:15:42 +0200

    Senaste ändringar

--- Serieboden.js
+++ Serieboden.js
@@ -1,9 +1,17 @@
-const Animationcss = document.getElementsByClassName("Animationcss");
-const formus = document.getElementsByClassName("formus");
+window.onload = function(e) {
+    let Animationcss = document.getElementById("Animationcss");
+console.log(Animationcss);
+const formus = document.getElementById("Formulr");
+console.log(formus);
 const Inloggning = document.getElementById("Inloggning");
 
-Animationcss.addEventListener("click", event =>{
+Animationcss.addEventListener("click", (event) =>{
+    if (formus.style.display == "none") {
+    formus.style.display = "inline-block";
+    //myButton.textContent = "Show";
+}
+else {
     formus.style.display = "none";
-    Inloggning.style.display = "none";
-    myButton.textContent = "Show";
+}
 });
+};
